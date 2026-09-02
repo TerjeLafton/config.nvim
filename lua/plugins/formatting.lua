2bb815b365e4f0603f665f75d5e9bd5f26cead68
Terje Lafton <terje@lafton.io>
2026-09-02 17:32:27 +0200

    feat: add haskell support

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,6 +3,7 @@ return {
   cmd = "ConformInfo",
   opts = {
     formatters_by_ft = {
+      haskell = { "fourmolu" },
       go = { "gofumpt" },
       lua = { "stylua" },
       json = { "biome" },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
     config = function()
       require("nvim-treesitter").install({
         -- Languages
+        "haskell",
         "go",
         "lua",
         "typescript",
